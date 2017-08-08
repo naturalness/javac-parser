e03a7c2ee9a99421d929a5b07e5b5b7489f81e82
Eddie Antonio Santos <easantos@ualberta.ca>
2017-08-08 11:14:18 -0600

    Streamline install from pypi.

--- javac_parser.py
+++ javac_parser.py
@@ -5,6 +5,7 @@ warn = logger.warn
 info = logger.info
 debug = logger.debug
 
+import sys
 import os
 import subprocess
 import time
@@ -15,9 +16,25 @@ import py4j
 from py4j.java_gateway import JavaGateway
 
 SOURCE_PATH = os.path.dirname(os.path.abspath(__file__))
-JAR_PATH = os.path.join(SOURCE_PATH, 
-                        "target",
-                        "lex-java-1.0-SNAPSHOT-jar-with-dependencies.jar")
+
+
+def find_jar_path():
+    "Tries to find where the lexer JAR is."
+    paths = []
+    jar_file = 'lex-java-1.0-SNAPSHOT-jar-with-dependencies.jar'
+    # setup.py local installation
+    paths.append(os.path.join(SOURCE_PATH, "share/javac-parser", jar_file))
+    # pip install
+    paths.append(os.path.join(sys.prefix, "share/javac-parser", jar_file))
+
+    for path in paths:
+        if os.path.exists(path):
+            return path
+    # Maven.
+    return os.path.join(SOURCE_PATH, "target", jar_file)
+
+JAR_PATH = find_jar_path()
+
 
 class Java(object):
     def __init__(self):
@@ -33,7 +50,8 @@ class Java(object):
         if os.path.isfile(JAR_PATH):
             pass
         else:
-            subprocess.check_call("mvn install:install-file -Dfile=${VIRTUAL_ENV}/share/py4j/py4j0.10.6.jar -DgroupId=py4j -DartifactId=py4j -Dversion=0.10.6 -Dpackaging=jar -DgeneratePom=true", shell=True)
+            py4j_jar = os.path.join(sys.prefix, 'share/py4j/py4j0.10.6.jar')
+            subprocess.check_call("mvn install:install-file -Dfile=" + py4j_jar + " -DgroupId=py4j -DartifactId=py4j -Dversion=0.10.6 -Dpackaging=jar -DgeneratePom=true", shell=True)
             subprocess.check_call("mvn package", shell=True)
             assert os.path.isfile(JAR_PATH)
         self.java_server = subprocess.Popen(
@@ -58,7 +76,7 @@ class Java(object):
                     raise
             else:
                 break
-    
+
     def __del__(self):
         #error("killing %i" % self.java_server.pid)
         if hasattr(self, 'gateway'):
@@ -68,14 +86,14 @@ class Java(object):
         if hasattr(self, 'java_server'):
             os.killpg(os.getpgid(self.java_server.pid), signal.SIGTERM)
             self.java_server.wait()
-        
+
     def get_num_parse_errors(self, java_source):
         return self.gateway.entry_point.getNumParseErrors(java_source)
-    
+
     @staticmethod
     def fix_extra_quotes(lexeme):
         l = tuple(lexeme)
-    
+
     def lex(self, java_source):
         # WARNING: assumes lines not in mac format
         lpos = 0
@@ -85,12 +103,12 @@ class Java(object):
             if lpos <= 0:
                 break
             lines.append(lpos)
-        
+
         def convert_position(i):
             line = bisect_right(lines, i)
             col = i-lines[line-1]
             return (line, col)
-        
+
         def convert(lexeme):
             t, v, s, e, string = tuple(lexeme)
             assert not (' \n\r\t' in string)
@@ -101,23 +119,23 @@ class Java(object):
                 convert_position(e),
                 string
                 )
-        
+
         return [convert(l) for l in self.gateway.entry_point.lex(java_source)]
-    
+
 import unittest
 class TestJava(unittest.TestCase):
     def setUp(self):
         self.java = Java()
-    
+
     def test_parse_ok(self):
         self.assertEqual(self.java.get_num_parse_errors(''), 0)
-        
+
     def test_parse_ok_class(self):
         s = """
             package ca.ualberta.cs;
-            
+
             import java.util.logging.Logger;
-            
+
             public class Bogus {
                 public int a = 1;
             }
@@ -127,29 +145,29 @@ class TestJava(unittest.TestCase):
     def test_parse_one_error(self):
         s = """
             package ca.ualberta.cs;
-            
+
             import java.util.logging.Logger;
-            
+
             public class Bogus {
                 public int a = 1;
                 public int b = ;
             }
         """
         self.assertEqual(self.java.get_num_parse_errors(s), 1)
-    
+
     def test_parse_two_error(self):
         s = """
             package ca.ualberta.cs;
-            
+
             import java.util.logging.Logger;
-            
+
             public class Bogus {
                 public int a = 1;
                 public int b = ;
-            
+
         """
         self.assertEqual(self.java.get_num_parse_errors(s), 2)
-    
+
     def test_lex(self):
         s = """
 package ca.ualberta.cs;
@@ -172,7 +190,7 @@ public class Bogus {
         self.assertEqual(lexed[0][3][1], len('package'))
         self.assertEqual(lexed[0][4], 'package')
         self.assertEqual(lexed[2][1], '.')
-        
+
     def test_lex_error(self):
         s = "String x = \\"
         lexed = self.java.lex(s)
@@ -183,6 +201,6 @@ public class Bogus {
 
     def tearDown(self):
         del self.java
-    
+
 if __name__ == '__main__':
     unittest.main()
