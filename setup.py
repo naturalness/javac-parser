f04829118a52fe29a46c513a0e97711d9a0a5f89
Eddie Antonio Santos <easantos@ualberta.ca>
2017-08-08 12:41:09 -0600

    Do not run mvn if it's not installed.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from distutils.spawn import find_executable
 
 import os
 import sys
@@ -12,16 +13,17 @@ PY4J_JAR = os.path.join(sys.prefix, 'share/py4j/py4j0.10.6.jar')
 JAR_PATH = os.path.join(SOURCE_PATH,
                         "target",
                         "lex-java-1.0-SNAPSHOT-jar-with-dependencies.jar")
-subprocess.check_call("mvn install:install-file -Dfile=" + PY4J_JAR + " -DgroupId=py4j -DartifactId=py4j -Dversion=0.10.6 -Dpackaging=jar -DgeneratePom=true", shell=True)
-subprocess.check_call("mvn package", shell=True)
+if find_executable('mvn'):
+      subprocess.check_call("mvn install:install-file -Dfile=" + PY4J_JAR + " -DgroupId=py4j -DartifactId=py4j -Dversion=0.10.6 -Dpackaging=jar -DgeneratePom=true", shell=True)
+      subprocess.check_call("mvn package", shell=True)
 assert os.path.isfile(JAR_PATH)
 
 
 setup(name='javac-parser',
-      version='0.1.2',
+      version='0.1.3',
       py_modules=['javac_parser'],
       data_files=[('share/javac-parser', [JAR_PATH])],
-      install_requires=['py4j>=0.10.6'],
+      install_requires=['py4j==0.10.6'],
 
       author='Eddie Antonio Santos, Joshua Charles Campbell',
       author_email='easantos@ualberta.ca, joshua2@ualberta.ca',
